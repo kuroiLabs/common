791eb148ea02172ec96040cb4c3b223a761ec886
kuro <kuro@kuroi.io>
2021-06-10 23:16:08 -0700

    add more methods to Packet classes

--- src/net/client/packet/packet.ts
+++ src/net/client/packet/packet.ts
@@ -18,20 +18,40 @@ export class Packet {
     return this.buffer
   }
 
-  public readByte(): byte {
-    const _byte: byte = new DataView(this.buffer, this.readPosition).getUint8(0)
+  public readUint8(): uint8 {
+    const _uint8: uint8 = new DataView(this.buffer, this.readPosition).getUint8(0)
     this.readPosition += Uint8Array.BYTES_PER_ELEMENT
+    return _uint8
+  }
+
+  public readUint8Array(_length: int): Uint8Array {
+    return new Uint8Array(this.buffer.slice(this.readPosition, _length || this.buffer.byteLength))
+  }
+
+  public writeUint8(_uint8: uint8): void {
+    const _view = new DataView(this.buffer)
+    _view.setUint8(this.writePosition, _uint8)
+    this.writePosition += Uint8Array.BYTES_PER_ELEMENT
+  }
+
+  public writeUint8Array(_uint8s: byte[] | Uint8Array): void {
+    _uint8s.forEach((_uint8: byte) => this.writeUint8(_uint8))
+  }
+
+  public readByte(): byte {
+    const _byte: byte = new DataView(this.buffer, this.readPosition).getInt8(0)
+    this.readPosition += Int8Array.BYTES_PER_ELEMENT
     return _byte
   }
 
-  public readBytes(_length?: int): Uint8Array {
-    return new Uint8Array(this.buffer.slice(this.readPosition, _length || this.buffer.byteLength))
+  public readByteArray(_length?: int): Int8Array {
+    return new Int8Array(this.buffer.slice(this.readPosition, _length || this.buffer.byteLength))
   }
 
   public writeByte(_byte: byte): void {
     const _view = new DataView(this.buffer)
-    _view.setUint8(this.writePosition, _byte)
-    this.writePosition += Uint8Array.BYTES_PER_ELEMENT
+    _view.setInt8(this.writePosition, _byte)
+    this.writePosition += Int8Array.BYTES_PER_ELEMENT
   }
 
   public writeBytes(_bytes: byte[] | Uint8Array): void {
@@ -44,10 +64,18 @@ export class Packet {
     return _uint32
   }
 
+  public readUint32Array(_length: int): Uint32Array {
+    return new Uint32Array(this.buffer.slice(this.readPosition, _length || this.buffer.byteLength))
+  }
+
   public writeUint32(_uint32: uint32): void {
     const _view = new DataView(this.buffer)
     _view.setUint32(this.writePosition, _uint32)
     this.writePosition += Uint32Array.BYTES_PER_ELEMENT
+  }
+
+  public writeUint32Array(_uint32s: uint32[] | Uint32Array): void {
+    _uint32s.forEach((_uint32: uint32) => this.writeUint32(_uint32))
   }
 
   public readInt32(): int32 {
@@ -56,10 +84,18 @@ export class Packet {
     return _int32
   }
 
+  public readInt32Array(_length: int): Int32Array {
+    return new Int32Array(this.buffer.slice(this.readPosition, _length || this.buffer.byteLength))
+  }
+
   public writeInt32(_int32: int32): void {
     const _view = new DataView(this.buffer)
     _view.setInt32(this.writePosition, _int32)
     this.writePosition += Int32Array.BYTES_PER_ELEMENT
+  }
+
+  public writeInt32Array(_int32s: int32[] | Int32Array): void {
+    _int32s.forEach((_uint32: int32) => this.writeUint32(_uint32))
   }
 
   public readFloat(): float {
@@ -72,6 +108,10 @@ export class Packet {
     const _view = new DataView(this.buffer)
     _view.setFloat32(this.writePosition, _float)
     this.writePosition += Float32Array.BYTES_PER_ELEMENT
+  }
+
+  public writeFloatArray(_floats: float[] | Float32Array): void {
+    _floats.forEach((_float: float) => this.writeFloat(_float))
   }
 
   public readString(end?: int): string {
--- src/net/server/packet/packet.ts
+++ src/net/server/packet/packet.ts
@@ -19,22 +19,41 @@ export class Packet {
     return this.buffer
   }
 
-  public readByte(): byte {
-    const _byte: byte = this.buffer.readUInt8(this.readPosition)
+  public readUint8(): uint8 {
+    const _uint8: uint8 = this.buffer.readUInt8(this.readPosition)
     this.readPosition += Uint8Array.BYTES_PER_ELEMENT
+    return _uint8
+  }
+
+  public readUint8Array(_length?: int): Uint8Array {
+    return new Uint8Array(this.buffer.slice(this.readPosition, _length || this.buffer.byteLength))
+  }
+
+  public writeUint8(_uint8: uint8): void {
+    this.buffer.writeUInt8(_uint8, this.writePosition)
+    this.writePosition += Uint8Array.BYTES_PER_ELEMENT
+  }
+
+  public writeUint8Array(_uint8s: uint8[] | Uint8Array): void {
+    _uint8s.forEach(_uint8 => this.writeUint8(_uint8))
+  }
+
+  public readByte(): byte {
+    const _byte: byte = this.buffer.readInt8(this.readPosition)
+    this.readPosition += Int8Array.BYTES_PER_ELEMENT
     return _byte
   }
 
-  public readBytes(_length?: int): Uint8Array {
-    return new Uint8Array(this.buffer.slice(this.readPosition, _length || this.buffer.byteLength))
+  public readBytes(_length?: int): Int8Array {
+    return new Int8Array(this.buffer.slice(this.readPosition, _length || this.buffer.byteLength))
   }
 
   public writeByte(_byte: byte): void {
     this.buffer.writeUInt8(_byte, this.writePosition)
-    this.writePosition += Uint8Array.BYTES_PER_ELEMENT
+    this.writePosition += Int8Array.BYTES_PER_ELEMENT
   }
 
-  public writeBytes(_bytes: byte[] | Uint8Array): void {
+  public writeByteArray(_bytes: byte[] | Int8Array): void {
     _bytes.forEach((_byte: byte) => this.writeByte(_byte))
   }
 
@@ -44,9 +63,17 @@ export class Packet {
     return _uint32
   }
 
+  public readUint32Array(_length: int): Uint32Array {
+    return new Uint32Array(this.buffer.slice(this.readPosition, _length || this.buffer.byteLength))
+  }
+
   public writeUint32(_uint32: uint32): void {
     this.buffer.writeUInt32BE(_uint32, this.writePosition)
     this.writePosition += Uint32Array.BYTES_PER_ELEMENT
+  }
+
+  public writeUint32Array(_uint32s: uint32[] | Uint32Array): void {
+    _uint32s.forEach(_uint32 => this.writeUint32(_uint32))
   }
 
   public readInt32(): int32 {
@@ -55,9 +82,17 @@ export class Packet {
     return _int32
   }
 
+  public readInt32Array(_length: int): Int32Array {
+    return new Int32Array(this.buffer.slice(this.readPosition, _length || this.buffer.byteLength))
+  }
+
   public writeInt32(_int32: int32): void {
     this.buffer.writeInt32BE(_int32, this.writePosition)
     this.writePosition += Int32Array.BYTES_PER_ELEMENT
+  }
+
+  public writeInt32Array(_int32s: int32[] | Int32Array): void {
+    _int32s.forEach(_int32 => this.writeInt32(_int32))
   }
 
   public readFloat(): float {
@@ -66,9 +101,17 @@ export class Packet {
     return _float
   }
 
+  public readFloats(_length: int): Float32Array {
+    return new Float32Array(this.buffer.slice(this.readPosition, _length || this.buffer.byteLength))
+  }
+
   public writeFloat(_float: float): void {
     this.buffer.writeFloatBE(_float, this.writePosition)
     this.writePosition += Float32Array.BYTES_PER_ELEMENT
+  }
+
+  public writeFloats(_floats: float[] | Float32Array): void {
+    _floats.forEach(_float => this.writeFloat(_float))
   }
 
   public readString(end?: int): string {
